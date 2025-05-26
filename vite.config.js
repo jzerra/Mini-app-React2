f888505e6f2344327292a5b3451e7e7f858ca851
jzerra <jzerra01@gmail.com>
2025-05-26 11:04:20 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: 'public',          // tell Vite to treat public/ as root
   plugins: [react()],
-  publicDir: 'public',
+  publicDir: '../public',  // public assets relative to root
+  build: {
+    outDir: '../dist'      // output to project root/dist
+  }
 })
